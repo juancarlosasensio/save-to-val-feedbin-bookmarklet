edfaabd2f2da9a70ee5ebb059516d3a6f16e2de8
Juan Carlos Asensio <37816105+juancarlosasensio@users.noreply.github.com>
2025-03-19 12:08:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+javascript:void(open(
+  'BOOKMARKS-URL/save?u='+encodeURIComponent(location.href)+'&t='
+  +encodeURIComponent(document.title)+
+  '&id=BOOKMARKS-CLIENT-ID&secret=BOOKMARKS-CLIENT-SECRET',
+  'Bookmark a link',
+  'width=400,height=450'
+))
